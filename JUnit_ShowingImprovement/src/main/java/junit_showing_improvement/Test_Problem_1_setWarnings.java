eb45ee1b62a6a4dea1f10692c34ec681fc2e09ea
1133859 <1133859@RICL-AI711304.us.ray.com>
2018-06-05 10:57:48 -0500

    Started to add the test cases.

--- JUnit_ShowingImprovement/src/main/java/junit_showing_improvement/Test_Problem_1_setWarnings.java
+++ JUnit_ShowingImprovement/src/main/java/junit_showing_improvement/Test_Problem_1_setWarnings.java
@@ -15,12 +15,37 @@ package junit_showing_improvement;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import junitparams.JUnitParamsRunner;
+
+@RunWith(JUnitParamsRunner.class)
 public class Test_Problem_1_setWarnings {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	private Problem_1_setWarnings WarnObject;
+	
+	private static final Object[] parametersForTest_Problem_1_setWarnings() {
+		return $(
+				//Parameters are: (1, 2, 3, 4, 5, 6)
+				//1 = distance, 2 = redLight, 3 = yellowLight, 4 = greenLight, 5 = buzzer, 6 = breaks
+				
+				//Test Case 1
+				$ (200.0, false, false, true, false, false),
+				/*//Test Case 2
+				$ (200.0, false, false, true, false, false),
+				//Test Case 3
+				$ (200.0, false, false, true, false, false),
+				//Test Case 4
+				$ (200.0, false, false, true, false, false),
+				//Test Case 5
+				$ (200.0, false, false, true, false, false),
+				//Test Case 6
+				$ (200.0, false, false, true, false, false),
+				//Test Case 7
+				$ (200.0, false, false, true, false, false),
+				//Test Case 8
+				$ (200.0, false, false, true, false, false)*/
+				
+				//);
 	}
-
 }
